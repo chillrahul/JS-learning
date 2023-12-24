7c1ee185dbf4a7001f9fe5096a9a8a5ceb6f499a
chillrahul <100218942+chillrahul@users.noreply.github.com>
2023-12-24 22:31:47 +0530

    Add files via upload
    conversion basics

---
+++ conversion.js
@@ -0,0 +1,55 @@
+// let score = "Rahul";
+
+// console.log(typeof score);
+// console.log(typeof score);
+// //console.log("hi");
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber); // point  to note it converts into number doesn't matter the value of variable
+// console.log(valueInNumber);
+
+// // "44" => 44
+// // "44cde" => NaN
+// // true =>1; false => 0
+
+// let isloggedIn = "rahul";
+// let bolleanIsLoggedIn = Boolean(isloggedIn);
+// console.log(typeof isloggedIn);
+// console.log(bolleanIsLoggedIn);
+
+// // 1=> true; 0 =>false
+// //"" => false
+// // "anything" => true
+
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+/************************* Operations **********************/
+
+let value = 3;
+let negValue = -value;
+//console.log(negValue);
+//2**3 == 2 power 3
+
+// let str1 = "Hello";
+// let str2 = " World";
+
+// let str3 = str1 + str2; //str1 - str2 = NaN
+// console.log(str3);
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2; //not preferred as not good readabolity
+
+let counter = 100;
+counter++;
+++counter;
+console.log(counter);
